aac85b32d33383e53e1a2ee384cc49c07df7c9fc
Trent <devtrentoh@gmail.com>
2017-08-24 00:15:29 +0900

    Apply custom environment file

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,14 @@
  */
 
 import * as express from "express";
+import * as dotenv from "dotenv";
 import * as path from "path";
 
 const app = express();
 
-app.set("port", 4000);
+dotenv.config({ path: ".env.crossfit" });
+
+app.set("port", process.env.PORT);
 app.listen(app.get("port"), () => {
   console.log(("  app running at http://localhost:%d in %s mode"), app.get("port"), app.get("env"));
 });
