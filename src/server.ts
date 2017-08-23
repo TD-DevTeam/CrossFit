90c1761cf8a746c39f3e461511225ade161a84ff
Trent <devtrentoh@gmail.com>
2017-08-24 00:07:30 +0900

    New simplified package setup

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,13 @@
 /**
  * Module dependencies.
  */
+
+import * as express from "express";
+import * as path from "path";
+
+const app = express();
+
+app.set("port", 4000);
+app.listen(app.get("port"), () => {
+  console.log(("  app running at http://localhost:%d in %s mode"), app.get("port"), app.get("env"));
+});
