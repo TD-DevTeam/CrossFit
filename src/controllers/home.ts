93903cfd1a1748f15390d32827d518372e7c0401
Trent <devtrentoh@gmail.com>
2017-08-26 21:39:56 +0900

    Minor comment edit

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -2,10 +2,10 @@ import { Request, Response } from "express";
 import Controller from "./controller";
 // Default classes & interfaces are to be loaded without {}.
 
-/**
- * GET /
- */
 export default class HomeController implements Controller {
+  /**
+  * GET /
+  */
   index(req: Request, res: Response): void {
     res.render("home", {
       title: "Crossfit!"
