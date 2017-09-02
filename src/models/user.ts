ce2f0d0851f5e8dab3efce4e88e30a7443358d53
Trent <devtrentoh@gmail.com>
2017-09-02 21:38:21 +0900

    Add user model

---
+++ src/models/user.ts
@@ -0,0 +1,33 @@
+import * as mongoose from "mongoose";
+import ModelGenerator from "./model";
+
+export default class UserModelGenerator implements ModelGenerator {
+  readonly schema = new mongoose.Schema({
+    email: { type: String, unique: true },
+    password: String
+  });
+
+  model: mongoose.Model<any>;
+
+  User() {
+    // TODO : Password Hashing with schemas
+
+    this.schema.methods.comparePassword = function (candidatePassword: string,
+      cb: (err: Error, isMatch: boolean) => any) {
+      if (candidatePassword === this.password) {
+        cb(undefined, true);
+      } else {
+        cb(undefined, false);
+      }
+    };
+
+    this.model = mongoose.model("User", this.schema);
+  }
+}
+
+
+export type UserModel = mongoose.Document & {
+  email: string,
+  password: string,
+  comparePassword: (candidatePassword: string, cb: (err: Error, isMatch: boolean) => any) => any
+};
