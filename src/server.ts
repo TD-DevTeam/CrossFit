b09e4be3d9c85f1d9b77d787a4a8fc5cbc6f0acd
Trent <devtrentoh@gmail.com>
2017-09-03 00:19:49 +0900

    auth file name changed

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ import * as passport from "passport";
 import * as validator from "express-validator";
 import * as flash from "express-flash";
 
-import { PassportAuth } from "./auth";
+import { PassportAuth } from "./passportauth";
 import { Router } from "./router";
 // path적을 필요 없이 자동 임포트 하도록 기여해도 좋을듯(자바처럼)
 
