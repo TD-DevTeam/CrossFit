9c1fcc3f49f8ccec9a7503b96de9f371154699d4
bellatoris <bellatoris@snu.ac.kr>
2017-09-10 12:53:11 +0900

    add exersice model

--- src/models/exercise.ts
+++ src/models/exercise.ts
@@ -2,4 +2,23 @@
  * Exercise schema
  */
 
+import * as mongoose from "mongoose";
+import ModelGenerator from "./model";
 
+export default class ExerciseModelGenerator implements ModelGenerator {
+  readonly schema = new mongoose.Schema({
+    name: { type: String, unique: true },
+  }, {
+    timestamps: true
+  });
+
+  model: mongoose.Model<ExerciseDocument>;
+
+  constructor() {
+    this.model = mongoose.model("Exercise", this.schmea);
+  }
+}
+
+export type ExerciseDocument = mongoose.Document & {
+  name: string,
+}
