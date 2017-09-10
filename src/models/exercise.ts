904beb9fc17802dc6390b123711ed295a1c05336
bellatoris <bellatoris@snu.ac.kr>
2017-09-10 15:55:04 +0900

    make exercise model generator singleton

--- src/models/exercise.ts
+++ src/models/exercise.ts
@@ -6,6 +6,16 @@ import * as mongoose from "mongoose";
 import ModelGenerator from "./model";
 
 export default class ExerciseModelGenerator implements ModelGenerator {
+  private static _instance: ExerciseModelGenerator;
+
+  public static get Instance(): ExerciseModelGenerator {
+    if (this._instance == undefined) {
+      this._instance = new this();
+    }
+
+    return this._instance;
+  }
+
   readonly schema = new mongoose.Schema({
     name: { type: String, unique: true },
   }, {
@@ -15,10 +25,10 @@ export default class ExerciseModelGenerator implements ModelGenerator {
   model: mongoose.Model<ExerciseDocument>;
 
   constructor() {
-    this.model = mongoose.model("Exercise", this.schmea);
+    this.model = mongoose.model("Exercise", this.schema);
   }
 }
 
 export type ExerciseDocument = mongoose.Document & {
   name: string,
-}
+};
