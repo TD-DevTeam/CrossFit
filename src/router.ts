b053e724a9abce637256d0bb67f01e9ecd328f2d
Trent <devtrentoh@gmail.com>
2017-09-17 16:57:15 +0900

    Logout page

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { Application } from "express";
 import HomeController from "./controllers/home";
 import LoginController from "./controllers/login";
+import LogoutController from "./controllers/logout";
 import ExerciseController from "./controllers/exercise";
 import SignupController from "./controllers/signup";
 
@@ -19,12 +20,14 @@ export class Router {
     // app route
     const homeController = HomeController.Instance;
     const loginController = LoginController.Instance;
+    const logoutController = LogoutController.Instance;
     const exerciseController = ExerciseController.Instance;
     const signupController = SignupController.Instance;
 
     app.get("/", homeController.get);
     app.get("/login", loginController.get);
     app.post("/login", loginController.post);
+    app.get("/logout", logoutController.get);
     app.get("/signup", signupController.get);
     app.post("/signup", signupController.post);
     app.get("/exercise", exerciseController.get);
